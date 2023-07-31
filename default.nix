4a8f811c2d99d3fb5648fb60787af2ba6d8195f7
Bayazid B. Halim <bayazid.bustami.halim@gmail.com>
2023-07-31 13:00:12 +0700

    actual first successful test
    actually read the sample config and pay attention to hierarchy!

--- default.nix
+++ default.nix
@@ -5,16 +5,17 @@
     system-manager.allowAnyDistro = true; # needed to bypass current restriction to NixOS and Ubuntu
     nixpkgs.hostPlatform = "x86_64-linux"; # needed by system-manager
 
-    # Packages to install
-    systemPackages = with pkgs; [
+    environment = {
+      # Packages to install
+      systemPackages = with pkgs; [
         ripgrep fd duperemove
       ];
-
-    # Create config files in /etc
-    environment.etc = {
-      "system-manager.conf.test".text = ''
-        it works!!!!!
-      '';
+      # Create config files in /etc
+      etc = {
+        "system-manager.conf.test".text = ''
+          it works!!!!!
+        '';
+      };
     };
   };
 }
