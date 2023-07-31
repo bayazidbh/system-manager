34551420175d131c7628e20083f90a09bbe85901
Bayazid B. Halim <bayazid.bustami.halim@gmail.com>
2023-07-31 11:42:50 +0700

    recurse home-manager

--- flake.nix
+++ flake.nix
@@ -1,14 +1,14 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
     system-manager = {
       url = "github:numtide/system-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, flake-utils, nixpkgs, system-manager }: {
+  outputs = { self, flake-utils, nixpkgs, system-manager }:
+  {
     systemConfigs.default = system-manager.lib.makeSystemConfig {
       modules = [
         ./pc/system.nix
